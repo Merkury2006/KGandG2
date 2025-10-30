54feb4242a56d2e09c9a5de40fdac0287496e94c
Merkury <elez123456789@yandex.ru>
2025-10-30 19:46:31 +0300

    осталось доделать дим шаг

--- src/main/java/org/example/kgandg2_1/BezierCurve.java
+++ src/main/java/org/example/kgandg2_1/BezierCurve.java
@@ -6,10 +6,13 @@ import javafx.scene.paint.Color;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BezierCurve {
     private List<Point2D> points = new ArrayList<>();
+    private Map<String, Double> binomialCache = new HashMap<>(100);
 
     public List<Point2D> getPoints() {
         return points;
@@ -21,19 +24,36 @@ public class BezierCurve {
 
     public void draw(PixelWriter pixelWriter, int widthCanvas, int heightCanvas, Color color) {
         if (points.size() < 2) return;
+
         int n = points.size() - 1;
         double step = calculateAdaptiveStep();
+        double[] coefficients = getBinomialCoefficients(n);
+
         double xPrev = points.getFirst().getX();
         double yPrev = points.getFirst().getY();
+
         for (double t = 0; t <= 1.0; t += step) {
             double xCur = 0;
             double yCur = 0;
+
+            double powT = 1.0;
+            double pow1_T = Math.pow(1.0 - t, n);
+
             for (int k = 0; k <= n; k ++) {
-                xCur += points.get(k).getX() * DrawUtils.getC(k, n) * Math.pow(t, k) * Math.pow(1-t, n - k);
-                yCur += points.get(k).getY() * DrawUtils.getC(k, n) * Math.pow(t, k) * Math.pow(1-t, n - k);
+                double basicFunction = coefficients[k] * powT * pow1_T;
+                xCur += points.get(k).getX() * basicFunction;
+                yCur += points.get(k).getY() * basicFunction;
+
+                if (t < 1.0) {
+                    pow1_T /= (1.0 - t);
+                }
+                powT *= t;
             }
+
             DrawUtils.drawLine((int) Math.round(xPrev),(int) Math.round(yPrev), (int) Math.round(xCur), (int) Math.round(yCur), pixelWriter, widthCanvas, heightCanvas, color);
-            xPrev = xCur; yPrev = yCur;
+
+            xPrev = xCur;
+            yPrev = yCur;
         }
     }
 
@@ -45,4 +65,23 @@ public class BezierCurve {
         return 0.002;
     }
 
+    private double[] getBinomialCoefficients(int n) {
+        double[] coefficients = new double[n + 1];
+        for (int k = 0; k <= n; k ++) {
+            String key = n + "," + k;
+            if (binomialCache.containsKey(key)) {
+                coefficients[k] = binomialCache.get(key);
+                continue;
+            }
+
+            double curRes = 1;
+            for (int i = 1; i <= k; i++) {
+                curRes *= (double) (n - k + i) / i;
+            }
+
+            binomialCache.put(n + "," + k, curRes);
+            coefficients[k] = curRes;
+        }
+        return coefficients;
+    }
 }
--- src/main/java/org/example/kgandg2_1/DrawUtils.java
+++ src/main/java/org/example/kgandg2_1/DrawUtils.java
@@ -4,54 +4,9 @@ import javafx.geometry.Point2D;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.paint.Color;
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class DrawUtils {
-    private static Map<Integer, Double> factorials = new HashMap<>();
-    static {
-        factorials.put(0, 1.0);
-        factorials.put(1, 1.0);
-        factorials.put(2, 2.0);
-        factorials.put(3, 6.0);
-        factorials.put(4, 24.0);
-        factorials.put(5, 120.0);
-        factorials.put(6, 720.0);
-        factorials.put(7, 5040.0);
-        factorials.put(8, 40320.0);
-        factorials.put(9, 362880.0);
-        factorials.put(10, 3628800.0);
-        factorials.put(11, 39916800.0);
-        factorials.put(12, 479001600.0);
-        factorials.put(13, 6227020800.0);
-        factorials.put(14, 87178291200.0);
-        factorials.put(15, 1307674368000.0);
-        factorials.put(16, 20922789888000.0);
-        factorials.put(17, 355687428096000.0);
-        factorials.put(18, 6402373705728000.0);
-        factorials.put(19, 121645100408832000.0);
-        factorials.put(20, 2432902008176640000.0);
-    }
-
-    private static double fact(int x) {
-        if (x < factorials.size()) {
-            return factorials.get(x);
-        }
-
-        double proizv = 1;
-        for (int i =1; i <= x; i ++) {
-            proizv *= i;
-        }
-        factorials.put(x, proizv);
-
-        return proizv;
-    }
-
-    public static double getC(int k, int n) {
-        return fact(n) / (fact(k) * fact(n - k));
-    }
-
     public static void drawLine(Point2D firstPoint, Point2D lastPoint, PixelWriter pixelWriter, int widthCanvas, int heightCanvas, Color color) {
         drawLine((int) firstPoint.getX(), (int) firstPoint.getY(), (int) lastPoint.getX(), (int) lastPoint.getY(), pixelWriter, widthCanvas, heightCanvas, color);
     }
